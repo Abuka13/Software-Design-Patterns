142236afaa986494cd972089590c02c9d88fe52a
Abuka13 <abukaozbekbay@gmail.com>
2025-10-04 20:19:08 +0500

    fixing

--- Assignment_4/src/Engine.java
+++ Assignment_4/src/Engine.java
@@ -1,14 +1,12 @@
 interface Engine {
     String getType();
 }
-
 class PetrolEngine implements Engine {
     @Override
     public String getType() {
         return "Petrol Engine";
     }
 }
-
 class ElectricEngine implements Engine {
     @Override
     public String getType() {
